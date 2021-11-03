4c8e6b18ea282d78532dcb8dd4c90950087f52db
bogdan <bogdan2435@gmail.com>
2021-11-03 15:57:36 +0200

    Challange1

---
+++ src/lab1_challange1/challange1.java
@@ -0,0 +1,21 @@
+package lab1_challange1;
+
+public class challange1 {
+
+    public static void Challange1(int number){
+        for(int i = 1; i <= 100; i++){
+            if((i % 3 == 0) && (i % 5 == 0))
+                System.out.print("FizzBuzz, ");
+            else if (i % 3 == 0)
+                System.out.print("Fizz, ");
+            else if (i % 5 == 0)
+                System.out.print("Buzz,");
+            else if (i % 7 == 0)
+                System.out.print("Rizz, ");
+            else if (i % 11 == 0)
+                System.out.print("Jazz, ");
+            else
+                System.out.print( i + ", ");
+        }
+    }
+}
---
+++ src/lab1_challange1/main.java
@@ -0,0 +1,7 @@
+package lab1_challange1;
+
+public class main {
+    public static void main(String[] args) {
+        challange1.Challange1(100);
+    }
+}
