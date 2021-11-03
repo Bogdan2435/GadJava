35668914112b894b541398ead8955acedfd058f9
bogdan <bogdan2435@gmail.com>
2021-11-03 17:22:03 +0200

    add challange 2

---
+++ src/lab2_challange/challange2.java
@@ -0,0 +1,51 @@
+package lab2_challange;
+
+public class challange2 {
+
+    public static String compute2(int number){
+        String s = "";int[] digits = Integer.toString(number).chars().map(c -> c-'0').toArray();
+
+        for(int i : digits){
+            if(i != 0){
+                String aux;
+                aux = Integer.toString(i);
+                s = s + i;
+            }
+
+            else
+                s = s + "*";
+        }
+        return s;
+    }
+
+    public static String compute(int number){
+        String s = "";
+
+        if(number % 3 == 0)
+            s = s + "Foo";
+        if(number % 5 == 0)
+            s = s + "Bar";
+        if(number % 7 == 0)
+            s = s + "Qix";
+
+
+        int[] digits = Integer.toString(number).chars().map(c -> c-'0').toArray();
+        // functia Integer.toString imi va stoca in vectorul de intregi "digits", cifrele variabilei intregi "number"
+
+        for(int i : digits){
+            if(i == 0)
+                s = s + "*";
+            if(i == 3)
+                s = s + "Foo";
+            if(i == 5)
+                s = s + "Bar";
+            if(i == 7)
+                s = s + "Qix";
+        }
+
+        if(s == null || s.length() == 0) // verific daca numarul se divide cu 3,5 sau 7 / contine 3,5 sau 7
+            s = s + number;
+
+        return s;
+    }
+}
---
+++ src/lab2_challange/main.java
@@ -0,0 +1,18 @@
+package lab2_challange;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        String s;
+        s = "";
+        s = s + challange2.compute(number);
+        if(s == null || s.length() == 0){
+            // daca numarul nu se divide cu 3,5 sau 7 si nici nu contine 3, 5 sau 7 atunci in valoarea pe care o returnam trebuie sa inlocuim cifra 0 cu *
+            s = s + challange2.compute2(number);
+        }
+        System.out.println(s);
+    }
+}
