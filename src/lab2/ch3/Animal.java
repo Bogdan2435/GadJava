3f2dc4c92c063da1278822796458441d5696cf1f
bogdan <bogdan2435@gmail.com>
2021-11-04 18:55:32 +0200

    create lab2ch3, not working

---
+++ src/lab2/ch3/Animal.java
@@ -0,0 +1,17 @@
+package lab2.ch3;
+
+public class Animal {
+    protected int legs;
+
+    protected Animal(int legs) {
+        this.legs = legs;
+    }
+
+    public eat(){
+    }
+
+    public walk(){
+    }
+
+
+}
---
+++ src/lab2/ch3/Spider.java
@@ -0,0 +1,10 @@
+package lab2.ch3;
+
+public class Spider extends Animal {
+    public Spider() {
+        legs = 8;
+    }
+
+}
+
+
